ebd46a147322a88fa0f1860bb6ccce1d49544106
Lum <linyux1104@163.com>
2024-01-03 17:14:59 +0800

    Update main.py

--- google_scholar_crawler/main.py
+++ google_scholar_crawler/main.py
@@ -17,7 +17,7 @@ with open(f'results/gs_data.json', 'w') as outfile:
 shieldio_data = {
   "schemaVersion": 1,
   "label": "citations",
-  "message": f"{author['citedby']}",
+  "message": f"{author['cites_per_year']}",
 }
 with open(f'results/gs_data_shieldsio.json', 'w') as outfile:
     json.dump(shieldio_data, outfile, ensure_ascii=False)
